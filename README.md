7700c8eaa871f799a00b6240ac5549cd6674ed2f
MPChege <mark.wanyoike@strathmore.edu>
2023-04-03 14:19:18 +0300

    completed project

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # phase-1-wk3-code-challenge
+# Movie Theater Ticketing System
+This is a movie theater ticketing system that allows customers to view available movies, movie details, and purchase tickets for showings.
+
+# Technologies Used
+This project uses the following technologies:
+
+HTML
+CSS
+JavaScript
+Express.js
+Node.js
+Installation
+To install and run this project, follow these steps:
+
+# Clone the repository to your local machine.
+Open a terminal and navigate to the project directory.
+Run npm install to install the necessary dependencies.
+Run node server.js to start the server.
+Open a web browser and navigate to http://localhost:3000.
+Usage
+When you open the app, you will see a list of available movies on the left side of the screen. Click on a movie to view its details, including the poster, title, runtime, showtime, and available tickets. The number of available tickets is calculated by subtracting the number of tickets sold from the theater's capacity.
+
+To purchase a ticket, click on the "Buy Ticket" button. If there are no tickets available, you will not be able to purchase a ticket.
+
+# Credits
+This project was created by Mark Paul.
--- index.js
+++ index.js
@@ -1,42 +1,59 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetchMovieDetails();
-    fetchMovieData();
-  });
-  
-  async function fetchMovieDetails() {
-    const response = await fetch("http://localhost:3000/films/1");
-    const movie = await response.json();
-    displayMovieDetails(movie);
-  }
-  
-  function displayMovieDetails(movie) {
+ // Get the movie data and populate the movie list and the first movie details
+ fetch('http://localhost:3000/films')
+ .then(response => response.json())
+ .then(data => {
+   const films = document.querySelector('#films');
+   films.innerHTML = ''; // Remove the placeholder element
+   data.forEach(film => {
+     const li = document.createElement('li');
+     li.classList.add('film', 'item');
+     li.innerText = film.title;
+     li.addEventListener('click', () => showMovieDetails(film));
+     films.appendChild(li);
+   });
+
+   // Show the first movie details by default
+   showMovieDetails(data[0]);
+ });
+
+// Show the movie details for a given movie object
+function showMovieDetails(movie) {
+    const poster = document.querySelector('#poster');
+    const title = document.querySelector('#title');
+    const runtime = document.querySelector('#runtime');
+    const showtime = document.querySelector('#showtime');
+    const tickets = document.querySelector('#tickets');
+    const buyTicket = document.querySelector('#buy-ticket');
+   
+    poster.src = movie.poster;
+    title.innerText = movie.title;
+    runtime.innerText = `Runtime: ${movie.runtime} min`;
+    showtime.innerText = `Showtime: ${movie.showtime}`;
     const availableTickets = movie.capacity - movie.tickets_sold;
-    const movieDetailsItemContainer = `
-      <div class="movie-details-item">
-        <img src="${movie.poster}">
-        <p>Title: ${movie.title}</p>
-        <p>Runtime: ${movie.runtime} minutes</p>
-        <p>Showtime: ${movie.showtime}</p>
-        <p>Available Tickets: ${availableTickets}</p>
-      </div>
-    `;
-    document.getElementById("movie-details").innerHTML = movieDetailsItemContainer;
-  }
-  
-  async function fetchMovieData() {
-    const response = await fetch("http://localhost:3000/films");
-    const movies = await response.json();
-    displayMovies(movies);
-  }
-  
-  function displayMovies(movies) {
-    const movieList = document.getElementById("films");
-    movieList.innerHTML = ""; // Remove any placeholder li elements
-    movies.forEach(movie => {
-      const li = document.createElement("li");
-      li.classList.add("film-item");
-      li.textContent = movie.title;
-      movieList.appendChild(li);
-    });
-  }
-  
+    tickets.innerText = `Tickets available: ${availableTickets}`;
+    buyTicket.disabled = availableTickets === 0;
+   
+    // Add a click listener to the "Buy Ticket" button
+    buyTicket.removeEventListener('click', buyTicketHandler);
+    buyTicket.addEventListener('click', buyTicketHandler);
+   
+    // Function to handle the "Buy Ticket" button click
+    const button = document.createElement("button");
+   button.textContent = "Buy Tickets";
+   button.addEventListener("click" , () =>  {
+      if(availableTickets>0){
+       availableTickets -- 
+      } 
+      if(availableTickets === 0){
+       button.textContent = "Sold Out"
+       button.disabled = true;
+      }
+
+      else{
+       button.textContent="Buy Tickets"
+      }
+     
+      availableTicketsElement.textContent = `Available Tickets: ${availableTickets}`;
+   });
+   }
+   
